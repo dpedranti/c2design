5dc5bf6f634b67c9bbed4afa87748b501a7f20a4
Derrick Pedranti <dpedranti@hotmail.com>
2017-08-20 20:54:37 -0400

    Global style tweaks; add Bootstrap accordion to Services page; add headings to Blog and Contact pages

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ServicesRoutingModule, routedComponents } from './services-routing.module';
 
 @NgModule({
-  imports: [ServicesRoutingModule],
+  imports: [ServicesRoutingModule, NgbModule],
   declarations: [routedComponents]
 })
 export class ServicesModule { }

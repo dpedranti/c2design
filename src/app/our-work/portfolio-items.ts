c37ecfbc5082b5b132eda3daae467f4a0a2bbd79
Derrick Pedranti <dpedranti@hotmail.com>
2018-11-24 17:26:58 -0800

    Move our work assets to global assets folder; add additional digital signage portfolio samples

--- src/app/our-work/portfolio-items.ts
+++ src/app/our-work/portfolio-items.ts
@@ -1,70 +1,80 @@
-import { Portfolio } from './portfolio';
+import { Portfolio } from '../../portfolio';
 
 export const WEB: Portfolio[] = [
   {
-    thumb: './assets/images/thumb-web-enrollment-home.jpg',
-    img: './assets/images/portfolio-web-enrollment-home.jpg',
+    thumb: '../../assets/images/thumb-web-enrollment-home.jpg',
+    img: '../../assets/images/portfolio-web-enrollment-home.jpg',
     title: 'Provider Enrollment Website'
   },
   {
-    thumb: './assets/images/thumb-web-enrollment-add-contact.png',
-    img: './assets/images/portfolio-web-enrollment-add-contact.png',
+    thumb: '../../assets/images/thumb-web-enrollment-add-contact.png',
+    img: '../../assets/images/portfolio-web-enrollment-add-contact.png',
     title: 'Provider Enrollment Web Page'
   },
   {
-    thumb: './assets/images/thumb-web-pin-home.jpg',
-    img: './assets/images/portfolio-web-pin-home.jpg',
+    thumb: '../../assets/images/thumb-web-pin-home.jpg',
+    img: '../../assets/images/portfolio-web-pin-home.jpg',
     title: 'Pharmacy Infomation Network Website'
   },
   {
-    thumb: './assets/images/thumb-web-find-provider.jpg',
-    img: './assets/images/portfolio-web-find-provider.jpg',
+    thumb: '../../assets/images/thumb-web-find-provider.jpg',
+    img: '../../assets/images/portfolio-web-find-provider.jpg',
     title: 'Find a Provider Web Page'
   },
   {
-    thumb: './assets/images/thumb-web-employee-referral.jpg',
-    img: './assets/images/portfolio-web-employee-referral.jpg',
+    thumb: '../../assets/images/thumb-web-employee-referral.jpg',
+    img: '../../assets/images/portfolio-web-employee-referral.jpg',
     title: 'Employee Referral Website'
   },
   {
-    thumb: './assets/images/thumb-web-internal-applicant.png',
-    img: './assets/images/portfolio-web-internal-applicant.png',
+    thumb: '../../assets/images/thumb-web-internal-applicant.png',
+    img: '../../assets/images/portfolio-web-internal-applicant.png',
     title: 'Internal Applicant Website'
   },
   {
-    thumb: './assets/images/thumb-web-onboarding-cafe.jpg',
-    img: './assets/images/portfolio-web-onboarding-cafe.jpg',
+    thumb: '../../assets/images/thumb-web-onboarding-cafe.jpg',
+    img: '../../assets/images/portfolio-web-onboarding-cafe.jpg',
     title: 'Onboarding Cafe Website'
   },
   {
-    thumb: './assets/images/thumb-web-food-finder.jpg',
-    img: './assets/images/portfolio-web-food-finder.jpg',
+    thumb: '../../assets/images/thumb-web-food-finder.jpg',
+    img: '../../assets/images/portfolio-web-food-finder.jpg',
     title: 'Food Finder Website'
   }
 ];
 
 export const MARKETING: Portfolio[] = [
   {
-    thumb: './assets/images/thumb-email-top-tower-new-year.jpg',
-    img: './assets/images/portfolio-email-top-tower-new-year.jpg',
+    thumb: '../../assets/images/thumb-email-top-tower-new-year.jpg',
+    img: '../../assets/images/portfolio-email-top-tower-new-year.jpg',
     title: 'Top of the Tower Happy New Year Email'
   },
   {
-    thumb: './assets/images/thumb-email-top-tower-anniversary.jpg',
-    img: './assets/images/portfolio-email-top-tower-anniversary.jpg',
+    thumb: '../../assets/images/thumb-email-top-tower-anniversary.jpg',
+    img: '../../assets/images/portfolio-email-top-tower-anniversary.jpg',
     title: 'Top of the Tower Happy Anniversary Email Template'
   }
 ];
 
 export const DIGITAL_SIGNAGE: Portfolio[] = [
   {
-    thumb: './assets/images/thumb-digital-signage-im-division-01.jpg',
-    video: './assets/media/im_dig_sign_division_01.mp4',
+    thumb: '../../assets/images/thumb-digital-signage-im-division-01.jpg',
+    video: '../../assets/media/im_dig_sign_division_01.mp4',
     title: 'Ingram Micro Digital Signage Division'
   },
   {
-    thumb: './assets/images/thumb-digital-signage-im-division-03.jpg',
-    video: './assets/media/im_dig_sign_division_03.mp4',
+    thumb: '../../assets/images/thumb-digital-signage-im-division-03.jpg',
+    video: '../../assets/media/im_dig_sign_division_03.mp4',
     title: 'Ingram Micro Digital Signage Division'
+  },
+  {
+    thumb: '../../assets/images/thumb-digital-signage-im-division-03.jpg',
+    video: '../../assets/media/im_dig_sign_ps_level3.mp4',
+    title: 'Ingram Micro Partner Smart'
+  },
+  {
+    thumb: '../../assets/images/thumb-digital-signage-im-division-03.jpg',
+    video: '../../assets/media/im_dig_sign_exigent_cake_boss.mp4',
+    title: 'Ingram Micro Digital Signage – Cake Boss'
   }
 ];
